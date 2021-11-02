778d4c5f0bb064c98107fde083facd114ccb2881
LautaroReinoso <lau.reinoso95@gmail.com>
2021-11-02 12:10:19 -0300

    commit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-const $form = document.querySelector ('#form')
-const $button = document.querySelector ('#mail')
+const $form = document.querySelector ('#form');
+const $button = document.querySelector ('#mail');
 
-$form.addEventListener('submit', handleSubmit )
+$form.addEventListener('submit', handleSubmit );
 
 function handleSubmit(event) {
     event.preventDefault()
     const form = new FormData (this)
     $button.setAttribute('href', `mailto:lautaroreinosoquinteto@gmail.com?subject=${form.get('nombre')}${form.get('e-email')}&body=${form.get(mensaje)}`)
     $button.click ()
-}
+};
